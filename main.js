bcdd7014799c8e6babe36525cec50f6984d142eb
Athul Sabu <88123015+athulcoder@users.noreply.github.com>
2022-07-30 16:09:17 +0530

    hhsdf

---
+++ main.js
@@ -0,0 +1,14 @@
+function links_out(key){
+    if (key==1){
+        window.open('https://www.python.org/','_blank')
+    }
+    else if (key==2){
+        window.open('https://www.javascript.com/','_blank')
+    }
+    else if (key==3){
+        window.open('https://www.php.net/','_blank')
+    }
+    else if (key==4){
+        window.open('https://www.cprogramming.com/','_blank')
+    }
+}
